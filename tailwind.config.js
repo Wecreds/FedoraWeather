a3896f2ae249ebd867a08f65b65c469530ca3b84
Wecreds <Wecredsbr@gmail.com>
2023-09-22 11:34:21 -0300

    feat(CSS): :art: Adicionando paleta de cores personalizada e fonte, junto com um favicon.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#798C8C",
+        "secundary-color":"#AEBFBE",
+        "tertiary-color": "tertiary",
+        "white": "#F2EFDF",
+        "black": "#161A1E"
+      },
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container
   },
   plugins: [],
 }
