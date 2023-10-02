b7592a6c700fe84412b1da1a2da84c27d5bd704e
Wecreds <Wecredsbr@gmail.com>
2023-10-02 12:52:36 -0300

    feat(Página de clima): :sparkles: Animação Loading
    Animação de carregamento da página de clima

---
+++ src/components/AnimatedPlaceholderComponent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-gradient-to-r from-slate-300 animate-pulse"> 
+        &nbsp;
+    </div>
+</template>
--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -2,10 +2,14 @@
   <div>
     <Suspense>
       <AsyncCityViewComponent />
+      <template #fallback>
+        <CityViewSkeletonComponent />
+      </template>
     </Suspense>
   </div>
 </template>
 
 <script setup>
 import AsyncCityViewComponent from '../components/AsyncCityViewComponent.vue'
+import CityViewSkeletonComponent from '../components/CityViewSkeletonComponent.vue';
 </script>
